4bb8073d95d281ed4cbb3e5e1a4fe2e8c0d106e5
leanderhughes <leanderhughes@gmail.com>
2021-04-30 20:58:53 +0900

    Finished Author routes

--- models/author.js
+++ models/author.js
@@ -1,10 +1,27 @@
 const mongoose = require('mongoose');
+const Book = require('./book');
 
 const authorSchema = new mongoose.Schema({
     name:{
         type: String,
         required: true
     }
+});
+
+authorSchema.pre('remove',function(next){
+    Book.find({
+        author:this.id
+    },(err,books)=>{
+        if(err){
+            next(err);
+            return;
+        }
+        if(books.length){
+            next(new Error("This author has books still"));
+            return;
+        }
+        next();
+    });
 });
 
 module.exports = mongoose.model('Author',authorSchema);
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const methodOverride = require('method-override');
 
 app.use(express.urlencoded({limit:'10mb',extended:false}));
 app.use(express.json());
@@ -17,6 +18,7 @@ app.set('view engine','ejs');
 app.set('views',__dirname+'/views');
 app.set('layout','layouts/layout');
 app.use(expressLayouts);
+app.use(methodOverride('_method'));
 app.use(express.static('public'));
 
 const mongoose = require('mongoose');
